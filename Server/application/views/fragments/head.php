78d160ff4def1ea3c2a5d26f2524150f8adace7c
Krispy Mallows <krispymallows@Slave.local>
2015-08-15 16:58:10 +0800

    moved mvc to server

---
+++ Server/application/views/fragments/head.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Smpclty</title>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>resources/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="resources/css/main.css">
+</head>
---
+++ Server/application/views/home.php
@@ -0,0 +1,67 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+
+<?php $this->load->view('fragments/head.php'); ?>
+<body>
+<!-- START CONTENT HERE -->
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-4">
+				<h2 class="text-center">
+					Smplcty
+				</h2>
+			</div>
+		</div>
+		<div class="space-big"></div>
+		<div class="row">
+			<div class="col-sm-12">
+				<div class="form-horizontal">
+					<div class="form-group">
+						<div class="col-sm-12">
+							<input type="text" class="form-control input-sm input-large" id="saearch" placeholder="Search for ...">
+						</div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-12 text-center">
+							<button class="btn btm-primary btn-primary">Search</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<ul>
+					<li>Suggested promo/shops/etc here</li>
+					<li>Suggested promo/shops/etc here</li>
+					<li>Suggested promo/shops/etc here</li>
+					<li>Suggested promo/shops/etc here</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+<script src="resources/js/jQuery/jQuery-2.1.4.min.js"></script>
+<script>
+	// $(function(){
+	// 	var data = {
+	// 		asdad : asdad
+	// 	};
+
+	// 	$.post("api/shop", {
+	// 		oper : 'save',
+	// 		data : JSON.stringify(data)
+	// 	}, function(response){
+	// 		if(typeof response['data'] != 'undefined'){
+
+	// 		}
+	// 	});
+	// })
+</script>
+</html>
